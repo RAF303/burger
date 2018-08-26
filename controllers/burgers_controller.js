7cab4acda6f0916ae170744097619c1552e940b0
Rafael Rodriguez <raf303@Rafaels-MacBook-Pro.local>
2018-08-26 14:20:58 -0600

    need to get burgers in devoured adding works

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,7 +19,6 @@ router.post("/api/burgers", function (req, res) {
     ], [
         req.body.burger, req.body.devoured
     ], function (result) {
-        // Send back the ID of the new quote
         res.json({
             id: result.insertId
         });
--- public/app.js
+++ public/app.js
@@ -1,23 +1,23 @@
 $(function () {
     console.log("**************** we made it in the function")
-    // $(".eaton").on("click", function (event) {
-    //     var id = $(this).data("id");
-    //     var newEaton = $(this).data("newEaten");
+    $(".eaton").on("click", function (event) {
+        var id = $(this).data("id");
+        var newEaton = $(this).data("newEaten");
 
-    //     var neweatonState = {
-    //         devoured: newEaton
-    //     };
+        var newEatonState = {
+            devoured: newEaton
+        };
 
-    //     $.ajax("/api/burgerss/" + id, {
-    //         type: "PUT",
-    //         data: newEatonState
-    //     }).then(
-    //         function () {
-    //             console.log("Eat staus", newEaton);
-    //             location.reload();
-    //         }
-    //     );
-    // });
+        $.ajax("/api/burgerss/" + id, {
+            type: "PUT",
+            data: newEatonState
+        }).then(
+            function () {
+                console.log("Eat staus", newEaton);
+                location.reload();
+            }
+        );
+    });
 
     $(".burger-order").on("submit", function (event) {
         event.preventDefault();
@@ -36,15 +36,15 @@ $(function () {
     })
 
 
-    $(".devour").on("click", function (event) {
-        var id = $(this).data("id");
-        $.ajax("/api/burgers/" + id, {
-            type: "DELETE"
-        }).then(
-            function () {
-                console.log("this buger was eaten", id);
-                location.reload();
-            }
-        )
-    })
+    // $(".devour").on("click", function (event) {
+    //     var id = $(this).data("id");
+    //     $.ajax("/api/burgers/" + id, {
+    //         type: "DELETE"
+    //     }).then(
+    //         function () {
+    //             console.log("this buger was eaten", id);
+    //             location.reload();
+    //         }
+    //     )
+    // })
 })
