0cd60b2c4251dcabd8d4cc816111ed44df17e5b1
Rafael Rodriguez <raf303@Rafaels-MacBook-Pro.local>
2018-08-26 13:56:34 -0600

    almost working

--- public/app.js
+++ public/app.js
@@ -1,10 +1,30 @@
 $(function () {
+    console.log("**************** we made it in the function")
+    // $(".eaton").on("click", function (event) {
+    //     var id = $(this).data("id");
+    //     var newEaton = $(this).data("newEaten");
+
+    //     var neweatonState = {
+    //         devoured: newEaton
+    //     };
+
+    //     $.ajax("/api/burgerss/" + id, {
+    //         type: "PUT",
+    //         data: newEatonState
+    //     }).then(
+    //         function () {
+    //             console.log("Eat staus", newEaton);
+    //             location.reload();
+    //         }
+    //     );
+    // });
+
     $(".burger-order").on("submit", function (event) {
         event.preventDefault();
 
         var newBurger = {
-            burger: $("patty").val().trim(),
-            devoured: $("[burger=devoured]: checked").val().trim()
+            burger: $("#patty").val().trim(),
+            // devoured: $("[burger=devoured]: checked").val().trim()
         }
         $.ajax("/api/burgers", {
             type: "POST",
